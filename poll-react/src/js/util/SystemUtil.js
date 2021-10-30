5e528d40c88adf5b69a4d73b5d2bdca253625f1e
mlmar <marcloussm@gmail.com>
2021-10-30 13:31:45 -0700

    dev mode off

--- poll-react/src/js/util/SystemUtil.js
+++ poll-react/src/js/util/SystemUtil.js
@@ -1,4 +1,4 @@
-const DEV = true;
+const DEV = false;
 
 export const SERVER_URL = DEV ? "http://localhost:3300" : "https://livesurvey-auth.herokuapp.com";
 export const SOCKET_URL = DEV ? "ws://localhost:3300" : "ws://livesurvey-auth.herokuapp.com";
